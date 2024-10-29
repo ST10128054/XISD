22c0f819cc9edc2c767050d3dbf46e28f342dd3a
moozie <103250149+ST10128054@users.noreply.github.com>
2024-10-29 12:08:31 +0200

    Added cart functions

--- addToCart.js
+++ addToCart.js
@@ -2,72 +2,83 @@ const product = [
     {
         id: 0,
         image: 'img/Sonia/armygreen_dress2.jpeg',
-        prodName: 'Army Green Dress',
-        price: 140,
+        title: 'Army Green Dress',
+        price: 120,
     },
     {
         id: 1,
         image: 'img/Sonia/beige_dress.jpeg',
-        prodName: 'Beige Dress',
+        title: 'Beige Dress',
         price: 120,
     },
     {
         id: 2,
         image: 'img/Sonia/black_dress.jpeg',
-        prodName: 'Black Dress',
+        title: 'Black Dress',
         price: 120,
+    },
+    {
+        id: 3,
+        image: 'img/Sonia/Black_fit.jpeg',
+        title: 'Black Outfit',
+        price: 150,
     }
 ];
-const catagories = [...new Set(product.map((item)=>{return item}))]
-let i = 0;
-document.getElementById('root').innerHTML = catagories.map((item)=>{
-    var{image, prodName, price} = item;
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
     return(
         `<div class='box'>
             <div class='img-box'>
-                <img class='images' srce=${image}></img>
+                <img class='images' src=${image}></img>
             </div>
         <div class='bottom'>
-        <p>${prodName}</p>
-        <h2>R ${price}.oo</h2>` +
-        "<button onclick='addToCart("+(i++)+")>Add to cart</button>" +
+        <p>${title}</p>
+        <h2>R ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
         `</div>
         </div>`
     )
-}).join('');
+}).join('')
 
 var cart =[];
 
-function addToCart(a){
-    cart.push({...catagories[a]});
-    displayCart();
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
 }
-
 function delElement(a){
     cart.splice(a, 1);
-    displayCart();
+    displaycart();
 }
 
-function displayCart(a){
+function displaycart(){
     let j = 0, total=0;
     document.getElementById("count").innerHTML=cart.length;
-    if(cart.lenght == 0){
+    if(cart.length==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
         document.getElementById("total").innerHTML = "R "+0+".00";
-    }else{
-        document.getElementById("cartItem").innerHTML = cart.map((items)=>{
-            var {image, prodName, price} = items;
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
             total=total+price;
             document.getElementById("total").innerHTML = "R "+total+".00";
             return(
-                `<div class='cart=item'>
-                    <div class='row-img'>
-                        <img class='rowimg' src=${image}>
-                    </div>
-                    <p style='font-size: 12px;'>${prodName}</p>
-                    <h2 style='font-size: 15px;'>R ${price}.00</h2>` +
-                    "<i class='fa-solid fa=trash' onclick='delElement("+ (j++) +")'</i></div>"
-            )
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>R ${price}.00</h2>`+
+                "<button onclick='delElement("+ (j++) +")'>Delete</i></div>"
+            );
         }).join('');
     }
+
+    
 }
